5a0cb553d60939aa45e912ec39a0e5255c3ccf32
NicFragale <nic.fragale@gmail.com>
2023-02-06 14:14:38 -0700

    Update NFZDEWHelper_BASICINSTALL.ps1

--- Utilities/OpenZITI-ZDEW/NFZDEWHelper_BASICINSTALL.ps1
+++ Utilities/OpenZITI-ZDEW/NFZDEWHelper_BASICINSTALL.ps1
@@ -1,4 +1,4 @@
-#  Powershell.exe -ExecutionPolicy Bypass -Command 'Invoke-WebRequest -Headers @{"""Cache-Control"""="""no-cache"""} -UseBasicParsing https://raw.githubusercontent.com/NicFragale/NetFoundry/main/Utilities/OpenZITI-ZDEW/NFZDEWHelper.ps1 -OutFile NFZDEWHelper.ps1; .\NFZDEWHelper.ps1 -conf https://raw.githubusercontent.com/NicFragale/NetFoundry/main/Utilities/OpenZITI-ZDEW//NFZDEWHelper_BASICINSTALL.ps1'
+#  Powershell.exe -ExecutionPolicy Bypass -Command 'Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/NicFragale/NetFoundry/main/Utilities/OpenZITI-ZDEW/NFZDEWHelper.ps1 -OutFile NFZDEWHelper.ps1; .\NFZDEWHelper.ps1 -conf https://raw.githubusercontent.com/NicFragale/NetFoundry/main/Utilities/OpenZITI-ZDEW//NFZDEWHelper_BASICINSTALL.ps1'
 $script:AutoUpdate      = $true
 $script:InputMode       = "install"
 $script:OverwriteInst   = $true
