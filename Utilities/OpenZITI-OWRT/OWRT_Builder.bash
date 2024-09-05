de6069df03739a3227e9c3adcfe497481d1a1659
NicFragale <nic.fragale@gmail.com>
2024-09-05 08:48:24 -0600

    Update OWRT_Builder.bash

--- Utilities/OpenZITI-OWRT/OWRT_Builder.bash
+++ Utilities/OpenZITI-OWRT/OWRT_Builder.bash
@@ -27,105 +27,107 @@ VCPKG_ROOT="${ZT_ROOT}/vcpkg"
 ################################################################################################################
 ZT_STEP="0" iCC="0" iBC="0" iVC="0" iVS="0"
 ZT_ADDLPKG=(
-	"autoconf" "automake" "autopoint" "build-essential"
-	"curl" "doxygen" "expect" "flex" "cppcheck" "gcovr" "gpg"
-	"graphviz" "libcap-dev" "libssl-dev" "libprotobuf-c-dev"
-	"libsystemd-dev" "libtool" "ninja-build" "lsb-release"
-	"pkg-config" "python3" "python3-pip" "software-properties-common"
-	"tar" "unzip" "wget" "zip" "zlib1g-dev" "gawk" "sed" "cmake"
+    "autoconf" "automake" "autopoint" "build-essential"
+    "curl" "doxygen" "expect" "flex" "cppcheck" "gcovr" "gpg"
+    "graphviz" "libcap-dev" "libssl-dev" "libprotobuf-c-dev"
+    "libsystemd-dev" "libtool" "ninja-build" "lsb-release"
+    "pkg-config" "python3" "python3-pip" "software-properties-common"
+    "tar" "unzip" "wget" "zip" "zlib1g-dev" "gawk" "sed" "cmake"
 )
 for ((i=0;i<100;i++)); do PRINT_PADDING+='          '; done
 function CPrint() {
-	local OUT_COLOR=(${1/:/ }) IN_TEXT="${2}" OUT_MAXWIDTH="${3}" OUT_SCREENWIDTH NL_INCLUDE i x z
-	shopt -s checkwinsize; (:); OUT_SCREENWIDTH="${COLUMNS:-$(tput cols 2>/dev/null || echo 80)}";
-	if [[ -z ${OUT_MAXWIDTH} ]]; then
-		OUT_MAXWIDTH="${OUT_SCREENWIDTH:-80}"
-		NL_INCLUDE='\n'
-	elif [[ ${OUT_MAXWIDTH} -eq "0" ]]; then
-		OUT_MAXWIDTH="${#IN_TEXT}"
-		NL_INCLUDE='\n'
-	elif [[ ${OUT_MAXWIDTH} -lt "0" ]]; then
-		OUT_MAXWIDTH="${#IN_TEXT}"
-		NL_INCLUDE=''
-	fi
-	[[ ${#IN_TEXT} -gt ${OUT_MAXWIDTH} ]] && IN_TEXT="${IN_TEXT:0:${OUT_MAXWIDTH}}"
-	if [[ ${OUT_COLOR} == "COLORTEST" ]]; then
-		OUT_MAXWIDTH="10"
-		for i in {1..107}; do
-			for x in {1..107}; do
-				[[ $((++z%(OUT_SCREENWIDTH/OUT_MAXWIDTH))) -eq 0 ]] && echo
-				IN_TEXT="${i}:${x}"
-				printf "\e[${i};${x}m%-${OUT_MAXWIDTH}s\e[1;0m" "${PRINT_PADDING:0:$(((OUT_MAXWIDTH/2)-${#IN_TEXT}/2))}${IN_TEXT}"
-			done
-		done && echo
-	else
-		printf "\e[${OUT_COLOR[0]};${OUT_COLOR[1]}m%-${OUT_MAXWIDTH}s\e[1;0m${NL_INCLUDE}" "${PRINT_PADDING:0:$(((OUT_MAXWIDTH/2)-${#IN_TEXT}/2))}${IN_TEXT}"
-	fi
+    local OUT_COLOR=(${1/:/ }) IN_TEXT="${2}" OUT_MAXWIDTH="${3}" OUT_SCREENWIDTH NL_INCLUDE i x z
+    shopt -s checkwinsize; (:); OUT_SCREENWIDTH="${COLUMNS:-$(tput cols 2>/dev/null || echo 80)}";
+    if [[ -z ${OUT_MAXWIDTH} ]]; then
+        OUT_MAXWIDTH="${OUT_SCREENWIDTH:-80}"
+        NL_INCLUDE='\n'
+    elif [[ ${OUT_MAXWIDTH} -eq "0" ]]; then
+        OUT_MAXWIDTH="${#IN_TEXT}"
+        NL_INCLUDE='\n'
+    elif [[ ${OUT_MAXWIDTH} -lt "0" ]]; then
+        OUT_MAXWIDTH="${#IN_TEXT}"
+        NL_INCLUDE=''
+    fi
+    [[ ${#IN_TEXT} -gt ${OUT_MAXWIDTH} ]] && IN_TEXT="${IN_TEXT:0:${OUT_MAXWIDTH}}"
+    if [[ ${OUT_COLOR} == "COLORTEST" ]]; then
+        OUT_MAXWIDTH="10"
+        for i in {1..107}; do
+            for x in {1..107}; do
+                [[ $((++z%(OUT_SCREENWIDTH/OUT_MAXWIDTH))) -eq 0 ]] && echo
+                IN_TEXT="${i}:${x}"
+                printf "\e[${i};${x}m%-${OUT_MAXWIDTH}s\e[1;0m" "${PRINT_PADDING:0:$(((OUT_MAXWIDTH/2)-${#IN_TEXT}/2))}${IN_TEXT}"
+            done
+        done && echo
+    else
+        printf "\e[${OUT_COLOR[0]};${OUT_COLOR[1]}m%-${OUT_MAXWIDTH}s\e[1;0m${NL_INCLUDE}" "${PRINT_PADDING:0:$(((OUT_MAXWIDTH/2)-${#IN_TEXT}/2))}${IN_TEXT}"
+    fi
 }
 function CheckCMake() {
-	local ZT_CMAKEVER SEMA="${1}" VerMinState="FALSE"
-	read -r -d '' -a ZT_CMAKEVER < <(\
-		awk '/^cmake version/ {
-			CMAKEVER=gensub(/^cmake version ([[:digit:]]+).([[:digit:]]+).([[:digit:]]+)$/,"\\1 \\2 \\3","1")
-			print CMAKEVER
-		}' <(cmake --version 2>/dev/null)
-	)
-	if [[ ${ZT_CMAKEVER[0]:-0} -gt ${ZT_CMAKEMINVER[0]} ]]; then
-		VerMinState="TRUE"
-	elif [[ ${ZT_CMAKEVER[0]:-0} -ge ${ZT_CMAKEMINVER[0]} ]] && [[ ${ZT_CMAKEVER[1]:-0} -ge ${ZT_CMAKEMINVER[1]} ]] && [[ ${ZT_CMAKEVER[2]:-0} -ge ${ZT_CMAKEMINVER[2]} ]]; then
-		VerMinState="TRUE"
-	fi
-	if [[ ${SEMA} == "INITIAL" ]]; then
-		if [[ ${VerMinState} == "FALSE" ]]; then
-			CPrint "30:45" "WARNING: CMake Version [${ZT_CMAKEVER[0]:-0}.${ZT_CMAKEVER[1]:-0}.${ZT_CMAKEVER[2]:-0} < 3.24.0]." "0"
-			apt remove -y --purge --auto-remove cmake
-			wget -O- https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null \
-				&& apt-add-repository -y "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs)" \
-				&& apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF7F09730B3F0A4
-		fi
-	elif [[ ${SEMA} == "FINAL" ]]; then
-		if [[ ${VerMinState} == "FALSE" ]]; then
-			CPrint "30:41" "ERROR: CMake Version Installed [${ZT_CMAKEMINVER[0]}.${ZT_CMAKEMINVER[1]}.${ZT_CMAKEMINVER[2]}]." "0"
-			CPrint "30:41" "ERROR: CMake Version Required [3.24.0+]." "0"
-			CPrint "30:41" "See [https://apt.kitware.com] for Latest Version." "0"
-			GTE ${ZT_STEP}
-		else
-			CPrint "30:47" "CMake Version OK:" "-1" && echo " ${ZT_CMAKEVER[0]:-0}.${ZT_CMAKEVER[1]:-0}.${ZT_CMAKEVER[2]:-0}"
-		fi
-	fi
+    local ZT_CMAKEVER SEMA="${1}" VerMinState="FALSE"
+    read -r -d '' -a ZT_CMAKEVER < <(\
+        awk '/^cmake version/ {
+            CMAKEVER=gensub(/^cmake version ([[:digit:]]+).([[:digit:]]+).([[:digit:]]+)$/,"\\1 \\2 \\3","1")
+            print CMAKEVER
+        }' <(cmake --version 2>/dev/null)
+    )
+    if [[ ${ZT_CMAKEVER[0]:-0} -gt ${ZT_CMAKEMINVER[0]} ]]; then
+        VerMinState="TRUE"
+    elif [[ ${ZT_CMAKEVER[0]:-0} -ge ${ZT_CMAKEMINVER[0]} ]] && [[ ${ZT_CMAKEVER[1]:-0} -ge ${ZT_CMAKEMINVER[1]} ]] && [[ ${ZT_CMAKEVER[2]:-0} -ge ${ZT_CMAKEMINVER[2]} ]]; then
+        VerMinState="TRUE"
+    fi
+    if [[ ${SEMA} == "INITIAL" ]]; then
+        if [[ ${VerMinState} == "FALSE" ]]; then
+            CPrint "30:45" "WARNING: CMake Version [${ZT_CMAKEVER[0]:-0}.${ZT_CMAKEVER[1]:-0}.${ZT_CMAKEVER[2]:-0} < 3.24.0]." "0"
+            apt remove -y --purge --auto-remove cmake
+            wget -O- https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null \
+                && apt-add-repository -y "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs)" \
+                && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF7F09730B3F0A4
+        fi
+    elif [[ ${SEMA} == "FINAL" ]]; then
+        if [[ ${VerMinState} == "FALSE" ]]; then
+            CPrint "30:41" "ERROR: CMake Version Installed [${ZT_CMAKEMINVER[0]}.${ZT_CMAKEMINVER[1]}.${ZT_CMAKEMINVER[2]}]." "0"
+            CPrint "30:41" "ERROR: CMake Version Required [3.24.0+]." "0"
+            CPrint "30:41" "See [https://apt.kitware.com] for Latest Version." "0"
+            GTE ${ZT_STEP}
+        else
+            CPrint "30:47" "CMake Version OK:" "-1" && echo " ${ZT_CMAKEVER[0]:-0}.${ZT_CMAKEVER[1]:-0}.${ZT_CMAKEVER[2]:-0}"
+        fi
+    fi
 }
 function GTE() {
-	CPrint "30:41" "ERROR: Early Exit at Step ${1}."
-	exit ${1}
+    CPrint "30:41" "ERROR: Early Exit at Step ${1}."
+    exit ${1}
 }
 
 ###################################################
 CPrint "30:46" "${MY_NAME:-UNSET NAME} - v${MY_VERSION:-UNSET VERSION} - ${MY_DESCRIPTION:-UNSET DESCRIPTION}"
 CPrint "30:42" "ZITI EDGE TUNNEL VERSION: ${ZT_TUNVER} (BRANCH:${ZT_TUNBRANCH})"
 CPrint "30:42" "OPENWRT VERSION: ${ZT_OWRT_VER:=UNKNOWN}"
-[[ ${ZT_OWRT_TARGET[0]} == "x86" ]] && [[ ${ZT_OWRT_TARGET[1]} == "64" ]] \
-	&& CPrint "30:42" "OPENWRT TARGET: ${ZT_OWRT_TARGET[0]}:${ZT_OWRT_TARGET[1]} (x64)" \
-	&& ZT_OWRT_TCINFO_X="x64" \
-	|| CPrint "30:42" "OPENWRT TARGET: ${ZT_OWRT_TARGET[0]}:${ZT_OWRT_TARGET[1]}"
+if [[ ${ZT_OWRT_TARGET[0]} == "x86" ]] && [[ ${ZT_OWRT_TARGET[1]} == "64" ]]; then
+    CPrint "30:42" "OPENWRT TARGET: ${ZT_OWRT_TARGET[0]}:${ZT_OWRT_TARGET[1]} (x64)"
+    ZT_OWRT_TCINFO_X="x64"
+else
+    CPrint "30:42" "OPENWRT TARGET: ${ZT_OWRT_TARGET[0]}:${ZT_OWRT_TARGET[1]}"
+fi
 
 ###################################################
 CPrint "30:43" "Begin Step $((++ZT_STEP)): Input Checking."
 if [[ ${ZT_TUNVER} == "UNKNOWN" ]] \
-	|| [[ ${ZT_OWRT_VER} == "UNKNOWN" ]] \
-	|| [[ ${ZT_OWRT_TARGET[0]} == "UNKNOWN" ]] \
-	|| [[ ${ZT_OWRT_TARGET[1]} == "UNKNOWN" ]]; then
-	CPrint "30:41" "Input Missing/Error - Please Check." "0"
-	GTE ${ZT_STEP}
+    || [[ ${ZT_OWRT_VER} == "UNKNOWN" ]] \
+    || [[ ${ZT_OWRT_TARGET[0]} == "UNKNOWN" ]] \
+    || [[ ${ZT_OWRT_TARGET[1]} == "UNKNOWN" ]]; then
+    CPrint "30:41" "Input Missing/Error - Please Check." "0"
+    GTE ${ZT_STEP}
 fi
 
 ###################################################
 CPrint "30:43" "Begin Step $((++ZT_STEP)): Create Staging Area."
 CPrint "30:47" "Location:" "-1" && echo " ${ZT_ROOT}"
 if [[ -d "${ZT_ROOT}" ]]; then
-	CPrint "30:45" "WARNING: Staging Area Already Exists." "0"
-	CPrint "30:45" "WARNING: Stop Program (CTRL+C) to Prevent Overwrite." "0"
-	sleep 5
-	rm -rf "${ZT_ROOT}" || GTE ${ZT_STEP}
+    CPrint "30:45" "WARNING: Staging Area Already Exists." "0"
+    CPrint "30:45" "WARNING: Stop Program (CTRL+C) to Prevent Overwrite." "0"
+    sleep 5
+    rm -rf "${ZT_ROOT}" || GTE ${ZT_STEP}
 fi
 git config --global advice.detachedHead false || GTE ${ZT_STEP}
 mkdir -vp "${ZT_ROOT}" || GTE ${ZT_STEP}
@@ -135,67 +137,67 @@ cd "${ZT_ROOT}" || GTE ${ZT_STEP}
 CPrint "30:43" "Begin Step $((++ZT_STEP)): Acquire Additional Software Part One."
 CheckCMake "INITIAL"
 apt-get update \
-	&& apt-get --yes --quiet --no-install-recommends install ${ZT_ADDLPKG[@]} \
-	&& apt-get --yes autoremove \
-	&& apt-get --yes autoclean \
-	|| GTE ${ZT_STEP}
+    && apt-get --yes --quiet --no-install-recommends install ${ZT_ADDLPKG[@]} \
+    && apt-get --yes autoremove \
+    && apt-get --yes autoclean \
+    || GTE ${ZT_STEP}
 CheckCMake "FINAL"
 
 ###################################################
 CPrint "30:43" "Begin Step $((++ZT_STEP)): Acquire ZITI EDGE TUNNEL Source [Version ${ZT_TUNVER}]."
 if [[ ${ZT_TUNVER:-latest} == "latest" ]]; then
-	IFS=$'\n' read -r -d '' -a ZT_ALLVERSIONS < <(\
-		wget "${ZT_TUNURL}/tags" -O- 2>/dev/null \
-		| awk '/tags/ {
-			if (match($0,/v[0-9].[0-9]+.[0-9]+/)) {
-				ALLVERSIONS[substr($0,RSTART,RLENGTH)]++}
-			} END {
-				for (EACHVERSION in ALLVERSIONS) {
-					gsub("v","",EACHVERSION)
-					print EACHVERSION
-				}
-			}' \
-		| sort -rnt '.' -k1,1 -k2,2 -k3,3
-	)
-	ZT_TUNVER="${ZT_ALLVERSIONS[0]}"
+    IFS=$'\n' read -r -d '' -a ZT_ALLVERSIONS < <(\
+        wget "${ZT_TUNURL}/tags" -O- 2>/dev/null \
+        | awk '/tags/ {
+            if (match($0,/v[0-9].[0-9]+.[0-9]+/)) {
+                ALLVERSIONS[substr($0,RSTART,RLENGTH)]++}
+            } END {
+                for (EACHVERSION in ALLVERSIONS) {
+                    gsub("v","",EACHVERSION)
+                    print EACHVERSION
+                }
+            }' \
+        | sort -rnt '.' -k1,1 -k2,2 -k3,3
+    )
+    ZT_TUNVER="${ZT_ALLVERSIONS[0]}"
 fi
 ZT_TUNVERARR=( ${ZT_TUNVER//\./ } )
 # VCPKG support began with ZITI-TUNNEL-SDK-C version 0.21.1.
 if [[ ${ZT_TUNVERARR[0]} -gt 0 ]]; then
-	ZT_USEVCPKG="TRUE"
+    ZT_USEVCPKG="TRUE"
 elif [[ ${ZT_TUNVERARR[0]} -eq 0 ]] && [[ ${ZT_TUNVERARR[1]} -gt 21 ]]; then
-	ZT_USEVCPKG="TRUE"
+    ZT_USEVCPKG="TRUE"
 elif [[ ${ZT_TUNVERARR[0]} -eq 0 ]] && [[ ${ZT_TUNVERARR[1]} -eq 21 ]] && [[ ${ZT_TUNVERARR[2]} -ge 1 ]]; then
-	ZT_USEVCPKG="TRUE"
+    ZT_USEVCPKG="TRUE"
 else
-	ZT_USEVCPKG="FALSE"
+    ZT_USEVCPKG="FALSE"
 fi
 git clone --branch "${ZT_TUNBRANCH:-v${ZT_TUNVER}}" "${ZT_TUNURL}" "${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}" || GTE ${ZT_STEP}
 mkdir -vp "${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}/build" || GTE ${ZT_STEP}
 
 ###################################################
 if [[ ${ZT_USEVCPKG} == "TRUE" ]]; then
-	CPrint "30:43" "Begin Step $((++ZT_STEP)): Acquire Additional Software Part Two - VCPKG Required."
-	git clone --branch "${VCPKG_VERSION}" "${VCPKG_URL}" "${VCPKG_ROOT}" || GTE ${ZT_STEP}
-	export VCPKG_FORCE_SYSTEM_BINARIES="yes"
-	${VCPKG_ROOT}/bootstrap-vcpkg.sh -disableMetrics || GTE ${ZT_STEP}
-	${VCPKG_ROOT}/vcpkg version >&1 >/dev/null || GTE ${ZT_STEP}
-	mkdir -vp "${VCPKG_ROOT}/custom-triplets" || GTE ${ZT_STEP}
+    CPrint "30:43" "Begin Step $((++ZT_STEP)): Acquire Additional Software Part Two - VCPKG Required."
+    git clone --branch "${VCPKG_VERSION}" "${VCPKG_URL}" "${VCPKG_ROOT}" || GTE ${ZT_STEP}
+    export VCPKG_FORCE_SYSTEM_BINARIES="yes"
+    ${VCPKG_ROOT}/bootstrap-vcpkg.sh -disableMetrics || GTE ${ZT_STEP}
+    ${VCPKG_ROOT}/vcpkg version >&1 >/dev/null || GTE ${ZT_STEP}
+    mkdir -vp "${VCPKG_ROOT}/custom-triplets" || GTE ${ZT_STEP}
 else
-	CPrint "30:43" "Skipping Step $((++ZT_STEP)): Acquire Additional Software Part Two - VCPKG Not Required."
+    CPrint "30:43" "Skipping Step $((++ZT_STEP)): Acquire Additional Software Part Two - VCPKG Not Required."
 fi
 
 ###################################################
 CPrint "30:43" "Begin Step $((++ZT_STEP)): Acquire OpenWRT SDK [Version ${ZT_OWRT_VER}] [Target ${ZT_OWRT_TARGET[0]}/${ZT_OWRT_TARGET[1]}]."
 ZT_OWRT_MUSLVER="$(wget -q "${ZT_OWRT_URL}/releases/${ZT_OWRT_VER}/targets/${ZT_OWRT_TARGET[0]}/${ZT_OWRT_TARGET[1]}" -O- \
-	| awk -F'-' '{
-		for (i=1;i<=NF;i++) {
-			if (match($i,"musl")){
-				print $i
-				exit
-			}
-		}
-	}'
+    | awk -F'-' '{
+        for (i=1;i<=NF;i++) {
+            if (match($i,"musl")){
+                print $i
+                exit
+            }
+        }
+    }'
 )"
 ZT_OWRT_SDK[0]="openwrt-sdk-${ZT_OWRT_VER}-${ZT_OWRT_TARGET[0]}-${ZT_OWRT_TARGET[1]}_gcc-${ZT_OWRT_MUSLVER}-x86_64"
 ZT_OWRT_SDK[1]="${ZT_OWRT_URL}/releases/${ZT_OWRT_VER}/targets/${ZT_OWRT_TARGET[0]}/${ZT_OWRT_TARGET[1]}/${ZT_OWRT_SDK[0]}.tar.xz"
@@ -249,7 +251,9 @@ ZT_CONFIG_CMAKEOPTS[((iCC++))]="-DTLSUV_TLSLIB=openssl"
 ZT_CONFIG_CMAKEOPTS[((iCC++))]="-DCMAKE_PREFIX_PATH=${ZT_OWRT_BUILDTOOLCHAIN}/${ZT_OWRT_TCINFO_X:-${ZT_OWRT_TCINFO[0]}}-linux"
 ZT_CONFIG_CMAKEOPTS[((iCC++))]="-DCMAKE_TOOLCHAIN_FILE=${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}/toolchains/${ZT_OWRT_TCINFO_X:-${ZT_OWRT_TCINFO[0]}}-openwrt.cmake"
 ZT_CONFIG_CMAKEOPTS[((iCC++))]="-DGIT_VERSION=${ZT_TUNVER}-0-0"
-ZT_CONFIG_CMAKEOPTS[((iCC++))]="--preset ci-linux-${ZT_OWRT_TCINFO_X:-${ZT_OWRT_TCINFO[0]}}-static-libssl"
+[[ ${ZT_OWRT_TCINFO_X:-${ZT_OWRT_TCINFO[0]}} == "aarch64" ]] \
+    && ZT_CONFIG_CMAKEOPTS[((iCC++))]="--preset ci-linux-arm64-static-libssl" \
+    || ZT_CONFIG_CMAKEOPTS[((iCC++))]="--preset ci-linux-${ZT_OWRT_TCINFO_X:-${ZT_OWRT_TCINFO[0]}}-static-libssl"
 ZT_CONFIG_CMAKEOPTS[((iCC++))]="-S ${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}"
 ZT_CONFIG_CMAKEOPTS[((iCC++))]="-B ${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}/build"
 ZT_BUILD_CMAKEOPTS[((iBC++))]="--build ${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}/build"
@@ -267,13 +271,13 @@ VCPKGSSLOPTS[((iVS++))]="--overlay-triplets=${VCPKG_ROOT}/custom-triplets"
 
 ###################################################
 if [[ ${ZT_USEVCPKG} == "TRUE" ]]; then
-	CPrint "30:43" "Begin Step $((++ZT_STEP)): Build Dependencies via VCPKG [Target ${ZT_OWRT_BUILDTARGET}]."
-	CPrint "30:47" "VCPKG SYNTAX:" "-1" && echo " ${VCPKG_ROOT}/vcpkg ${VCPKGOPTS[@]}"
-	${VCPKG_ROOT}/vcpkg ${VCPKGOPTS[@]} || GTE ${ZT_STEP}
-	CPrint "30:47" "VCPKG SYNTAX:" "-1" && echo " ${VCPKG_ROOT}/vcpkg ${VCPKGSSLOPTS[@]}"
-	${VCPKG_ROOT}/vcpkg ${VCPKGSSLOPTS[@]} || GTE ${ZT_STEP}
+    CPrint "30:43" "Begin Step $((++ZT_STEP)): Build Dependencies via VCPKG [Target ${ZT_OWRT_BUILDTARGET}]."
+    CPrint "30:47" "VCPKG SYNTAX:" "-1" && echo " ${VCPKG_ROOT}/vcpkg ${VCPKGOPTS[@]}"
+    ${VCPKG_ROOT}/vcpkg ${VCPKGOPTS[@]} || GTE ${ZT_STEP}
+    CPrint "30:47" "VCPKG SYNTAX:" "-1" && echo " ${VCPKG_ROOT}/vcpkg ${VCPKGSSLOPTS[@]}"
+    ${VCPKG_ROOT}/vcpkg ${VCPKGSSLOPTS[@]} || GTE ${ZT_STEP}
 else
-	CPrint "30:43" "Skipping Step $((++ZT_STEP)): Build Dependencies via VCPKG - Not Required."
+    CPrint "30:43" "Skipping Step $((++ZT_STEP)): Build Dependencies via VCPKG - Not Required."
 fi
 
 ###################################################
@@ -288,7 +292,7 @@ CPrint "30:43" "Begin Step $((++ZT_STEP)): Pre-Build Modifications."
 sed -i '/Ziti C SDK version/i ZITI_LOG(INFO, "Welcome to Ziti - OpenWRT Edition [v'"${BUILD_VERSION}"']");' "${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}/build/_deps/ziti-sdk-c-src/library/utils.c"
 sed -i '/# if ! __GNUC_PREREQ(4,9)/,+2d' $(find "${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}" -name "metrics.h") || GTE ${ZT_STEP}
 if [[ ${ZT_USEVCPKG} != "TRUE" ]]; then
-	cp -vr "/usr/include/sodium.h" "/usr/include/sodium" "${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}/build/_deps/ziti-sdk-c-src/includes" || GTE ${ZT_STEP}
+    cp -vr "/usr/include/sodium.h" "/usr/include/sodium" "${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}/build/_deps/ziti-sdk-c-src/includes" || GTE ${ZT_STEP}
 fi
 # Note: This is required until a patch is made to implicitly include time support in the model.
 sed -i '/#include <sys\/time.h>/d ; /#define ZITI_SDK_MODEL_SUPPORT_H/a #include <sys\/time.h>' "${ZT_ROOT}/ziti-tunnel-sdk-c-${ZT_TUNVER}/build/_deps/ziti-sdk-c-src/includes/ziti/model_support.h"
